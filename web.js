158ba3e91f8f31bc4ae3e259487a7a3c09e8b2ec
derwinmcgeary <derwinmcgeary+web@gmail.com>
2013-07-28 02:44:47 +0600

    first prototype of tee shirt stuff

--- web.js
+++ web.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var fs = require('fs');
 var HOMEPAGE = 'index.html';
+process.env.PWD = process.cwd()
 
 var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
+app.use(express.static(process.env.PWD + '/'));
+/* app.get('/', function(request, response) {
+	console.log(request);
   response.send(fs.readFileSync(HOMEPAGE).toString('utf-8'));
 });
-
+*/
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
